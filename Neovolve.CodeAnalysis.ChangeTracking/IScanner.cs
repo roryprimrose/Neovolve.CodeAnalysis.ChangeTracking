9e6dbb566522b5bcc6d80fd70bd613b60d625708
Rory Primrose <rory@neovolve.com>
2019-11-01 16:26:04 +1100

    Added initial scanner structure

---
+++ Neovolve.CodeAnalysis.ChangeTracking/IScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+
+    public interface IScanner
+    {
+        Task<IAsyncEnumerable<NodeDefinition>> FindDefinitions(IAsyncEnumerable<SyntaxNode> nodes);
+    }
+}
---
+++ Neovolve.CodeAnalysis.ChangeTracking/Scanner.cs
@@ -0,0 +1,23 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.Extensions.Logging;
+
+    public class Scanner : IScanner
+    {
+        private readonly ILogger _logger;
+
+        public Scanner(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<IAsyncEnumerable<NodeDefinition>> FindDefinitions(IAsyncEnumerable<SyntaxNode> nodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
