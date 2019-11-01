376a37794f7edea82a33d26549b65deeb0b132ac
Rory Primrose <rory@neovolve.com>
2019-11-01 14:34:32 +1100

    Add project files.

---
+++ Neovolve.CodeAnalysis.ChangeTracking/AttributeDefinition.cs
@@ -0,0 +1,7 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking
+{
+    public class AttributeDefinition : NodeDefinition
+    {
+        public string Declaration { get; set; }
+    }
+}
---
+++ Neovolve.CodeAnalysis.ChangeTracking/INodeResolver.cs
@@ -0,0 +1,11 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking
+{
+    using Microsoft.CodeAnalysis;
+
+    public interface INodeResolver<TN, out TD> where TN : SyntaxNode
+    {
+        TD Resolve(SyntaxNode node);
+
+        bool EvaluateChildren { get; }
+    }
+}
