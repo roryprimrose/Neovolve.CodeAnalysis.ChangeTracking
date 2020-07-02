349522223b1ace081aa496c249f893a7208b7b9d
Rory Primrose <rory@neovolve.com>
2020-07-02 22:32:36 +1000

    Updated packages

--- Neovolve.CodeAnalysis.ChangeTracking.UnitTests/Extensions.cs
+++ Neovolve.CodeAnalysis.ChangeTracking.UnitTests/Extensions.cs
@@ -1,15 +1,12 @@
 ﻿namespace Neovolve.CodeAnalysis.ChangeTracking.UnitTests
 {
     using System;
-    using EnsureThat;
     using Newtonsoft.Json;
 
     internal static class Extensions
     {
         public static T JsonClone<T>(this T value) where T : class
         {
-            Ensure.Any.IsNotNull(value, nameof(value));
-
             var data = JsonConvert.SerializeObject(value);
 
             var type = typeof(T);
