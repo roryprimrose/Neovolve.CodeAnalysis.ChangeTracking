38834e99585818a1b9e6d42b200cc27d634fdcf3
Rory Primrose <rory@neovolve.com>
2021-08-21 16:04:38 +1000

    Added enum comparison

---
+++ Neovolve.CodeAnalysis.ChangeTracking.UnitTests/Comparers/EnumComparerTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking.UnitTests.Comparers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using ModelBuilder;
+    using Neovolve.CodeAnalysis.ChangeTracking.Comparers;
+    using Neovolve.CodeAnalysis.ChangeTracking.Models;
+    using Neovolve.CodeAnalysis.ChangeTracking.Processors;
+    using Neovolve.CodeAnalysis.ChangeTracking.UnitTests.TestModels;
+    using NSubstitute;
+    using Xunit;
+
+    public class EnumComparerTests : Tests<EnumComparer>
+    {
+        [Fact]
+        public void CompareMatchReturnsResultsFromEnumMemberProcessor()
+        {
+            var oldItem = new TestEnumDefinition().Set(x => x.Members = new[] { new TestEnumMemberDefinition() });
+            var newItem = oldItem.JsonClone();
+            var changeType = Model.Create<SemVerChangeType>();
+            var match = new ItemMatch<IEnumDefinition>(oldItem, newItem);
+            var options = Model.Create<ComparerOptions>();
+            var result = new ComparisonResult(changeType, oldItem.Members.Last(), newItem.Members.Last(),
+                Guid.NewGuid().ToString());
+            var results = new List<ComparisonResult>
+            {
+                result
+            };
+
+            Service<IEnumMemberMatchProcessor>().CalculateChanges(oldItem.Members, newItem.Members, options)
+                .Returns(results);
+
+            var actual = SUT.CompareMatch(match, options);
+
+            actual.Should().BeEquivalentTo(results);
+        }
+
+        [Fact]
+        public void ThrowsExceptionWhenCreatedWithNullEnumMemberProcessor()
+        {
+            var accessModifiersComparer = Substitute.For<IAccessModifiersComparer>();
+            var attributeProcessor = Substitute.For<IAttributeMatchProcessor>();
+
+            Action action = () => new EnumComparer(null!, accessModifiersComparer, attributeProcessor);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ Neovolve.CodeAnalysis.ChangeTracking/Comparers/EnumComparer.cs
@@ -0,0 +1,30 @@
+﻿namespace Neovolve.CodeAnalysis.ChangeTracking.Comparers
+{
+    using System;
+    using Neovolve.CodeAnalysis.ChangeTracking.Models;
+    using Neovolve.CodeAnalysis.ChangeTracking.Processors;
+
+    public class EnumComparer : BaseTypeComparer<IEnumDefinition>, IEnumComparer
+    {
+        private readonly IEnumMemberMatchProcessor _enumMemberMatchProcessor;
+
+        public EnumComparer(IEnumMemberMatchProcessor enumMemberMatchProcessor,
+            IAccessModifiersComparer accessModifiersComparer,
+            IAttributeMatchProcessor attributeProcessor) : base(accessModifiersComparer, attributeProcessor)
+        {
+            _enumMemberMatchProcessor = enumMemberMatchProcessor
+                                        ?? throw new ArgumentNullException(nameof(enumMemberMatchProcessor));
+        }
+
+        protected override void EvaluateChildElementChanges(ItemMatch<IEnumDefinition> match, ComparerOptions options,
+            IChangeResultAggregator aggregator)
+        {
+            base.EvaluateChildElementChanges(match, options, aggregator);
+
+            var results =
+                _enumMemberMatchProcessor.CalculateChanges(match.OldItem.Members, match.NewItem.Members, options);
+
+            aggregator.AddResults(results);
+        }
+    }
+}
